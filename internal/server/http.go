5b45a27da6024dbd225e4482fea855d782932d62
Nguyen Manh Tuan Anh <anhng.dev@gmail.com>
2022-05-04 10:31:29 +0700

    Update CustomForwardResponse, CustomErrorHandler

--- internal/server/http.go
+++ internal/server/http.go
@@ -1,12 +1,20 @@
 package server
 
 import (
+	"context"
+	"errors"
+	"io"
+	"net/http"
+
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	commonpb "github.com/xdorro/base-project-proto/protos/v1/common"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/grpclog"
+	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 
 	authpb "github.com/xdorro/base-project-proto/protos/v1/auth"
 	userpb "github.com/xdorro/base-project-proto/protos/v1/user"
@@ -32,6 +40,8 @@ func (s *Server) newHTTPServer(tlsCredentials credentials.TransportCredentials, 
 				Resolver:        nil,
 			},
 		}),
+		runtime.WithErrorHandler(CustomErrorHandler),
+		runtime.WithForwardResponseOption(CustomForwardResponse),
 	)
 
 	opts := []grpc.DialOption{
@@ -66,4 +76,60 @@ func (s *Server) newHTTPServer(tlsCredentials credentials.TransportCredentials, 
 	if err = authpb.RegisterAuthServiceHandler(s.ctx, s.httpServer, conn); err != nil {
 		log.Panic("proto.RegisterAuthServiceHandler(): %w", zap.Error(err))
 	}
+}
+
+// CustomErrorHandler handles the error from the backend to the client.
+func CustomErrorHandler(
+	ctx context.Context, _ *runtime.ServeMux, marshaler runtime.Marshaler, w http.ResponseWriter, _ *http.Request,
+	err error,
+) {
+	val, ok := runtime.RPCMethod(ctx)
+	if !ok {
+		log.Error("runtime.RPCMethod(): %w", zap.Error(err))
+	}
+	log.Info("CustomHTTPResponse", zap.String("method", val))
+
+	// return Internal when Marshal failed
+	const fallback = `{"error": true, "message": "failed to marshal error message"}`
+
+	var customStatus *runtime.HTTPStatusError
+	if errors.As(err, &customStatus) {
+		err = customStatus.Err
+	}
+
+	s := status.Convert(err)
+	pb := s.Proto()
+
+	w.Header().Del("Trailer")
+	w.Header().Del("Transfer-Encoding")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+
+	encryptedResponse := &commonpb.CommonResponse{
+		Error:   true,
+		Message: pb.GetMessage(),
+	}
+	responseBody, merr := marshaler.Marshal(encryptedResponse)
+	if merr != nil {
+		grpclog.Infof("Failed to marshal error message %q: %v", s, merr)
+		if _, err = io.WriteString(w, fallback); err != nil {
+			grpclog.Infof("Failed to write response: %v", err)
+		}
+		return
+	}
+
+	if _, err = w.Write(responseBody); err != nil {
+		grpclog.Infof("Failed to write response: %v", err)
+	}
+}
+
+// CustomForwardResponse forwards the response from the backend to the client.
+func CustomForwardResponse(_ context.Context, w http.ResponseWriter, _ proto.Message) error {
+	headers := w.Header()
+	if location, ok := headers["Grpc-Metadata-Location"]; ok {
+		w.Header().Set("Location", location[0])
+		w.WriteHeader(http.StatusFound)
+	}
+
+	return nil
 }
